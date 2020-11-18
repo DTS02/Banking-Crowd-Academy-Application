d2d9be6d50bf1f58563606f3157952c920d11e35
dbsSensei <dimas@codingbee.org>
2020-11-18 22:46:27 +0700

    Menambah event pada firstName lastName dan tombol submit

--- client/src/Registrasi/Registrasi.jsx
+++ client/src/Registrasi/Registrasi.jsx
@@ -4,20 +4,39 @@ import './Registrasi.css';
 import { Dropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-
 class Registrasi extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {firstName: '', lastName:''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+        handleChange = (e) => {
+        const {id , value} = e.target   
+        this.setState(prevState => ({
+            ...prevState,
+            [id] : value
+            }))
+        }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.firstName + " " + this.state.lastName);
+        event.preventDefault();
+      }
+
+
     render() {
         return (
             <Router>
             <Fragment>
 
-                <div className="form-add-post">
+                <form className="form-add-post" onSubmit={this.handleSubmit}>
                     <p className='headtitle'>Yuk daftar<br /> biar bisa belajar<br /> bareng kita</p>
                     <div className='nama-satu'>
-                        <input type="text" name="title" placeholder="Nama Depan Kamu" ></input>
-                        <input type="text" name="title"  placeholder="Nama Belakang Kamu" ></input>
+                        <input id="firstName" type="text" name="title" placeholder="Nama Depan Kamu" value={this.state.firstName} onChange={this.handleChange} />
+                        <input id="lastName" type="text" name="title"  placeholder="Nama Belakang Kamu" value={this.state.lastName} onChange={this.handleChange} />
                     </div>
                     <div className='nama-dua'>
                     <input type="text" name="title" placeholder="Username Kamu" ></input>
@@ -37,10 +56,9 @@ class Registrasi extends Component {
                                 </Dropdown>
 
                     </div>
-                                                    
-                    <button className="btn-submit" >Daftar</button>
+                    <input className="btn-submit" type="submit" value="Daftar" />                                
                     <p className='kelogin'>Kamu sudah mendaftar? Yuk langsung  <Link to ="/login">masuk!</Link></p>
-                </div>
+                </form>
             
 
             </Fragment>
